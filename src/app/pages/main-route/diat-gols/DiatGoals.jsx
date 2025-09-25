5644f0a1fd7efc2b7817ea837241f286c7f518b2
nurulla-hasan <nurullahasan.dev@gmail.com>
2025-09-25 18:28:25 +0600

    update diatgoals

--- src/app/pages/main-route/diat-gols/DiatGoals.jsx
+++ src/app/pages/main-route/diat-gols/DiatGoals.jsx
@@ -1,16 +1,32 @@
 import PageLayout from '@/app/layout/PageLayout';
 import PageHeader from '@/components/common/page-header/PageHeader';
 import { useParams } from 'react-router-dom';
-import { mockRecipes } from '@/lib/mockData';
-import { useState } from 'react';
 import RecipeCard from '@/components/Recipes/recipe-card/RecipeCard';
+import useSmartFetchHook from '@/hooks/useSmartFetchHook';
+import { useGetRecipeByCategoryQuery } from '@/redux/feature/home/homeApi';
+import CustomPagination from '@/components/common/custom-pagination/CustomPagination';
+import CategoryPageSkeleton from '@/components/skeleton/category/CategoryPageSkeleton';
+import Error from '@/components/common/error/Error';
+import NoData from '@/components/common/no-data/NoData';
 
 const DiatGoals = () => {
-    const [recipes] = useState(mockRecipes);
-    const { slug: diatGols } = useParams();
-    console.log(diatGols);
-    const formattedSlug = diatGols.charAt(0).toUpperCase() + diatGols.slice(1);
+    const { slug: diatGoals } = useParams();
+    const formattedSlug = diatGoals.charAt(0).toUpperCase() + diatGoals.slice(1);
 
+
+    const {
+        currentPage,
+        setCurrentPage,
+        totalPages,
+        items: recipes,
+        isLoading,
+        isError,
+    } = useSmartFetchHook(useGetRecipeByCategoryQuery, { resultsKey: "result" }, { category: diatGoals });
+
+    if (isLoading) {
+        return <CategoryPageSkeleton />;
+    }
+    
     const breadcrumbs = [
         { name: 'Home', href: '/' },
         { name: formattedSlug },
@@ -22,11 +38,37 @@ const DiatGoals = () => {
                 breadcrumbs={breadcrumbs}
                 title={formattedSlug}
             />
-            <PageLayout paddingSize="compact">
+            <PageLayout
+                pagination={
+                    totalPages > 1 && (
+                        <div className="absolute bottom-4 left-0 right-0">
+                            <CustomPagination
+                                currentPage={currentPage}
+                                totalPages={totalPages}
+                                onPageChange={setCurrentPage}
+                            />
+                        </div>
+                    )
+                }
+                paddingSize="compact">
                 <div className="grid gap-6 grid-cols-1">
-                    {recipes.filter(recipe => recipe.category.toLowerCase() === diatGols).map(recipe => (
-                        <RecipeCard key={recipe.id} recipe={recipe} from={formattedSlug} fromPath={`/diat-gols/${diatGols}`} showCartButton={true} />
-                    ))}
+                    {isError ? (
+                        <Error size="perfect" msg="Something went wrong" />
+                    ) : (
+                        recipes.length === 0 ? (
+                            <NoData size="perfect" msg="No recipes found" />
+                        ) : (
+                            recipes.map(recipe => (
+                                <RecipeCard
+                                    key={recipe._id}
+                                    recipe={recipe}
+                                    from={formattedSlug}
+                                    fromPath={`/diat-gols/${diatGoals}`}
+                                    showCartButton={true}
+                                />
+                            ))
+                        )
+                    )}
                 </div>
             </PageLayout>
         </>
--- src/components/home/DietGoals.jsx
+++ src/components/home/DietGoals.jsx
@@ -1,51 +1,50 @@
 import React from 'react';
 import PageLayout from '@/app/layout/PageLayout';
-import { Carousel, CarouselContent, CarouselItem } from "@/components/ui/carousel";
-import { Button } from '@/components/ui/button';
-import { ChevronLeft, ChevronRight } from 'lucide-react';
 import Title from '../ui/Title';
 import { Link } from 'react-router-dom';
-import { diatGoals } from '@/lib/mockData';
+import { Slice, UtensilsCrossed, Cake } from 'lucide-react';
+
+const diatGoals = [
+    {
+        name: 'Weight & Muscle',
+        category: "weight_and_muscle",
+        icon: <Slice className="w-12 h-12" />
+    },
+    {
+        name: 'Weight Loss',
+        category: "weight_loss",
+        icon: <UtensilsCrossed className="w-12 h-12" />
+    },
+    {
+        name: 'Muscle Gain',
+        category: "muscle_gain",
+        icon: <Cake className="w-12 h-12" />
+    }
+];
 
 const DietGoals = () => {
-    const [api, setApi] = React.useState(null);
-
     return (
         <PageLayout>
-            <div className="flex justify-between items-center mb-8">
-                <Title title="Diet Goals" />
-                <div className="flex gap-2">
-                    <Button variant="outline" size="icon" onClick={() => api?.scrollPrev()}>
-                        <ChevronLeft className="h-4 w-4" />
-                        <span className="sr-only">Previous slide</span>
-                    </Button>
-                    <Button variant="outline" size="icon" onClick={() => api?.scrollNext()}>
-                        <ChevronRight className="h-4 w-4" />
-                        <span className="sr-only">Next slide</span>
-                    </Button>
-                </div>
+            <div className="text-center mb-12">
+                <Title title="Our Top Categories" />
             </div>
 
-            <Carousel setApi={setApi} opts={{ align: "start", loop: true }} className="w-full">
-                <CarouselContent>
-                    {diatGoals.map((goal, i) => (
-                        <CarouselItem key={i} className="sm:basis-1/2 lg:basis-1/5 mx-auto">
-                            <Link
-                                to={`/diat-gols/${goal.name.replace(" ", "-").toLowerCase()}`}
-                                className="group"
-                                key={i}
-                            >
-                                <div className="group-hover:scale-95 transition-all duration-300 bg-secondary rounded-2xl p-6 flex flex-col items-center justify-center shadow-sm aspect-square">
-                                    <div className="bg-card p-6 rounded-lg flex-grow flex items-center justify-center w-full">
-                                        {goal.icon}
-                                    </div>
-                                    <p className="mt-4 text-lg font-semibold text-primary dark:text-foreground">{goal.name}</p>
-                                </div>
-                            </Link>
-                        </CarouselItem>
-                    ))}
-                </CarouselContent>
-            </Carousel>
+            <div className="grid grid-cols-2 md:grid-cols-3 gap-8">
+                {diatGoals.map((goal, i) => (
+                    <Link
+                        to={`/diat-gols/${goal.category.toLowerCase()}`}
+                        className="group"
+                        key={i}
+                    >
+                        <div className="group-hover:scale-95 transition-all duration-300 bg-secondary rounded-2xl p-6 flex flex-col items-center justify-center shadow-sm aspect-square">
+                            <div className="bg-card p-6 rounded-lg flex-grow flex items-center justify-center w-full">
+                                {goal.icon}
+                            </div>
+                            <p className="mt-4 text-lg font-semibold text-primary dark:text-foreground">{goal.name}</p>
+                        </div>
+                    </Link>
+                ))}
+            </div>
         </PageLayout>
     );
 };
