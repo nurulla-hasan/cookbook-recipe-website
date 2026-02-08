0a9b17e61f2d877a29762d51c8f57e3a157f7110
nurulla-hasan <nurullahasan.dev@gmail.com>
2026-02-08 17:50:25 +0600

    chore: change dev and preview server ports to 5173

--- vite.config.js
+++ vite.config.js
@@ -46,14 +46,14 @@ export default defineConfig({
   // DEV SERVER (npm run dev)
   server: {
     host: '0.0.0.0',
-    port: 5174,
+    port: 5173,
     strictPort: true,
   },
 
   // PREVIEW SERVER (npm run preview)
   preview: {
     host: '0.0.0.0',
-    port: 5174,
+    port: 5173,
     allowedHosts: [
       'koumanisdietapp.com',
       'www.koumanisdietapp.com',
